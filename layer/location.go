0f5890fdae1bc7f17bf50911cfdc627e071ff01b
TedyChang <teddychangstar@gmail.com>
2022-07-08 21:11:28 +0900

    common validate

--- layer/location.go
+++ layer/location.go
@@ -10,15 +10,26 @@ type Location struct {
 }
 
 func (r Location) String() (string, error) {
-	if r.X == 0 || r.Y == 0 {
-		return "", err("0,0 is nil")
+	err1 := r.validate()
+	if err1 != nil {
+		return "", err1
 	}
+
 	return fmt.Sprintf("%f,%f", r.X, r.Y), nil
 }
 
 func (r Location) Draw() ([]Location, error) {
-	if r.X == 0 || r.Y == 0 {
-		return []Location{}, err("0,0 is nil")
+	err1 := r.validate()
+	if err1 != nil {
+		return nil, err1
 	}
+
 	return []Location{r}, nil
+}
+
+func (r Location) validate() error {
+	if r.X == 0 || r.Y == 0 {
+		return err("0,0 is nil")
+	}
+	return nil
 }
